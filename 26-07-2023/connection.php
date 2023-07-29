044f66d2f21f6a67392d87098007504dd11f7555
Skintrphoenix <randy.phoenix.098@gmail.com>
2023-07-29 18:38:01 +0700

    fix first load

--- 26-07-2023/connection.php
+++ 26-07-2023/connection.php
@@ -4,7 +4,7 @@ if(!function_exists("refresh")){
     function refresh(){
         $mysql = new \mysqli("localhost", "skin", "skin");
         $mysql->multi_query(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "db.sql"));
-        $mysql->close();
+        $mysql->next_result();
     }
 }
 
